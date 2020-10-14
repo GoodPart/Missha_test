a84d1717753cebc00a9732f12abadb09abf41669
GoodPart <40373934+GoodPart@users.noreply.github.com>
2020-10-14 15:09:17 +0900

    푸시푸시

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -9,6 +9,31 @@ const freeSwiper = new Swiper('.free-swiper', {
 })
 
 const mySwiper = new Swiper('.swiper-container', {
-    speed: 400,
-    spaceBetween: 100
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
 });
+
+
+
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+    
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+  document.getElementById("count").innerHTML =  (hours < 10 ? '0'+hours :hours) + " : "
+  + (minutes < 10 ? '0'+minutes :minutes) + " : " + (seconds < 10 ? '0'+seconds :seconds) ;
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("count").innerHTML = "ERROR";
+  }
+}, 1000);
