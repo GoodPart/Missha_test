acd167810425383d3168d9448b6d1b0aa7118689
GoodPart <40373934+GoodPart@users.noreply.github.com>
2020-10-20 17:33:36 +0900

    푸시

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -13,8 +13,11 @@ const mySwiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
         type: 'bullets',
       },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 });
-
 
 
 var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
@@ -37,3 +40,54 @@ var x = setInterval(function() {
     document.getElementById("count").innerHTML = "ERROR";
   }
 }, 1000);
+
+
+
+var header = document.querySelector(".module--navigation");
+var btns = header.getElementsByClassName("navigation-list");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(e) {
+    e.preventDefault()
+  var current = document.getElementsByClassName("is_active");
+  current[0].className = current[0].className.replace(" is_active", "");
+  this.className += " is_active";
+  });
+}
+
+
+// const moduleNavigation = document.querySelector(".module--navigation");
+// const moduleList = document.querySelector(".navigation-list >.module-link");
+// console.log("moduleNavigation",moduleNavigation)
+// console.log("moduleList",moduleList)
+
+// moduleNavigation.children.forEach((btn)=> {
+// })
+
+
+
+// for(var i = 0; i<moduleNavigation.children.length; i++) {
+//   // console.log(moduleNavigation.children[i])
+//   moduleNavigation.children[i].addEventListener('click', function(e){
+//     e.preventDefault()
+//     // console.log("moduleNavigation.children[e]", e.path[1].classList.contains("is_active"))
+//     var current = document.querySelector(".is_active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " is_active";
+
+
+
+//     // if(e.path[1].classList.contains("is_active")) {
+//     //   // for(var j = 0; j<moduleNavigation.children.length; j++){
+//     //   //   e.path[j].classList.remove("is_active")
+//     //   //   console.log("rr")
+//     //   // }
+//     //   // console.log("add class", e.path)
+//     //   // console.log("remove class", e.target)
+//     // }else {
+//     //   e.path[1].classList.add("is_active")
+      
+      
+//     // }
+//   })
+
+// }
