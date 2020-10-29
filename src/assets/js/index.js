9b641de551dd3aa8d70f4ac4a55615a436b0a7fc
GoodPart <40373934+GoodPart@users.noreply.github.com>
2020-10-29 17:30:45 +0900

    푸시

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,21 +1,25 @@
 import Swiper from 'swiper';
 import Masonry from 'masonry-layout'
 
-var nestCurrent, nestTotal;
 
+var nestCurrent = 0;
+
+const paginationName = ['시즌 이벤트', '신제품', '구매사은품', '1+1품목', '기획전'];
+let nestSlideLength = [];
+
+
+console.log("nestCurrent-초기값", nestCurrent)
 
 
 const freeSwiper = new Swiper('.free-swiper', {
     spaceBetween: 30,
     freeMode: true,
     slidesPerView: 'auto'
+
 })
 
-
-
-
-
 const nested = new Swiper('.nest-container', {
+  parallax: true,
   nested: true,
   pagination: {
     el: '.swiper-pagination',
@@ -23,48 +27,73 @@ const nested = new Swiper('.nest-container', {
   },
   on:{
     init: function() {
-      console.log("init", nested)
-      console.log("init", document.querySelector(".nest-container"))
-      
+      //init function
     },
     slideChange: function() {
-      // console.log("change", )
-      console.log("change2", this.activeIndex)
+
+      nestCurrent = this.activeIndex+1;
+      const index = this.activeIndex;
+      console.log(mySwiper.activeIndex, index);
       
-      return nestCurrent = this.activeIndex;
+      document.querySelector(".count-"+mySwiper.activeIndex).innerHTML= nestCurrent
+      // nestCurrent = 0;
+      
       
     }
   }
 });
 
-// nested.init(
-  // console.log("nested.current", nested),
-  // console.log("nested-slides.length", nested.slides.length),
+
+
+
+function appendSlide(slides, index) {
+  const mainTab = document.querySelector(".main-visual-tab");
+  const slide = document.createElement("li")
+  const tabTitle = slides.attributes[1].value;
+  const maxCount = mainSwiperSlides.length;
+  const nestSlide = slides.querySelectorAll(".swiper-slide");
+  const nestSlideMaxCount = nestSlide.length;
+
+  //카운트 백틱
+  const count =`
+  <span class="count-container">
+    <span class="count-${index}">${nestCurrent+1}</span> / <span>${nestSlideMaxCount}</span>
+  </span>
+  `
+
+  slide.className = "tab-list"
+  slide.innerHTML=tabTitle+ count
+  slide.style.width = 100/ maxCount+'%'
+  mainTab.appendChild(slide)
   
-  // console.log("nestTotal", nestTotal)
-// )
+}
 
-// nested.updateSlides(
-//   console.log("nested updated",nested)
-// )
 
-// console.log("bullets",mySwiper.pagination.bullets[1])
+const mainSwiperSlides = document.querySelectorAll(".main-swiper > .swiper-wrapper >.swiper-slide");
+const mainVisualTab = document.querySelector(".main-visual-tab");
+mainSwiperSlides.forEach((slides, index) => {
+  const nestSlides = slides.querySelectorAll(".swiper-slide");
+  
 
-const mainSwiperPagination = document.querySelector(".main-swiper__pagination")
-const msps= mainSwiperPagination.querySelectorAll("span")
-const testNestedNumber = [4,2,5,1,0];
-// console.log("mainSwiperPagination", mainSwiperPagination)
-// console.log("msps", msps)
+  appendSlide(slides, index);
+  // addName(slides);
 
-// msps.forEach((msp, index)=> {
-//   console.log("nestCurrent", nestCurrent)
-//   // msp.innerHTML = testNestedNumber[index];
-//   // msp.innerHTML = `${nestCurrent} / ${testNestedNumber[index]}`;
-//   // nestCurrent
-//   console.log("asdasdasdasdasd", nestCurrent)
-// })
-console.log("nestCurrentnestCurrent",nested.nestCurrent)
-// console.log("nestCurrentnestCurrent",nested[1].slides)
+  console.log("mySwiper 슬라이드 갯수", slides.length)
+
+  if(nestSlides.length === 0) {
+    nestSlideLength.push(0)
+  }else {
+
+    nestSlideLength.push(nestSlides.length)
+
+
+
+  }
+  console.log("nestSlideLength", nestSlideLength)
+})
+
+
+
 
 const mySwiper = new Swiper('.main-swiper', {
   navigation: {
@@ -73,15 +102,58 @@ const mySwiper = new Swiper('.main-swiper', {
   },
   pagination: {
     el: '.swiper-pagination',
-    // type: 'bullets',
     clickable: true,
-    renderBullet: function(index, className, nestCurrent) {
-      return '<span class="' + className + '">' + nestCurrent +'/'+ testNestedNumber[index] + '</span>';
+    renderBullet: function(index, className) {
+      return '<span alt='+paginationName[index]+' class="' + className + '">'+ paginationName[index] + nestCurrent +'/'+ nestSlideLength[index] + '</span>';
     }
   },
-  // nested: true
+  on:{
+    init: function() {
+      const tab = document.querySelectorAll(".tab-list");
+      const countContainer = document.querySelectorAll(".count-container");
+
+      tab[this.activeIndex].classList.add("is_active")
+      countContainer[this.activeIndex].classList.add("is_active")
+
+    },
+    slideChange: function() {
+
+      document.querySelector(".main-visual-tab")
+      const index = this.activeIndex;
+
+      const tabArray = document.querySelectorAll(".tab-list");
+      const countContainer = document.querySelectorAll(".count-container");
+
+
+
+      tabArray.forEach((tab, index)=> {
+        if(this.activeIndex ===index) {
+          tab.classList.add("is_active")
+          countContainer[index].classList.add("is_active")
+        }else {
+          tab.classList.remove("is_active")
+          countContainer[index].classList.remove("is_active")
+        }
+      })
+
+    }
+  }
 });
 
+const tt = document.querySelectorAll(".tab-list");
+tt.forEach((t,index)=> {
+  t.addEventListener("click", function(e) {
+    mySwiper.slideTo(index, 500);
+    console.log("hit", e)
+    console.log("hit--t", t)
+  })
+})
+
+
+
+/*
+///////////////////////////////////////////////////
+*/
 
 
 var countDownDate = new Date("Jan 5, 2021 11:20:00").getTime();
@@ -105,46 +177,6 @@ var x = setInterval(function() {
   }
 }, 1000);
 
-const moduleNavigation = document.querySelector(".module--navigation");
-const moduleList = moduleNavigation.querySelectorAll(".navigation-list");
-// console.log("moduleNavigation",moduleNavigation)
-// console.log("moduleList",moduleList)
-
-moduleList.forEach( module=> {
-  module.addEventListener("click", e=> {
-    e.preventDefault()
-      // console.log("module", module)
-
-      // console.log("asdasdasd", e)
-      // console.log("asdasdasd", e.path)
-
-      // moduleList.forEach(module => {
-      //   setTimeout(()=> {
-
-      //     module.classList.contains("is_active")? module.classList.remove("is_active") : module.classList.add("is_active")
-      //   },1000)
-      // })
-      
-        // for(var i = 0; i<moduleList.length; i++) {
-        //   if(moduleList[i].classList.contains("is_active") ) {
-        //     module.classList.remove("is_active")
-        //     // console.log("active 제거")
-        //   }else {
-        //     // console.log("active 추가")
-        //     module.classList.add("is_active")
-        //   }
-
-        // }
-
-      
-      // ? module.classList.remove("is_active") : module.classList.add("is_active")
-
-  })
-
-  
-})
-
-
 
 
 //리뷰 아이디 **표시
@@ -164,7 +196,7 @@ const ranks = document.querySelectorAll(".rank")
 // console.log("rank", rank)
 ranks.forEach(rank => {
   const rankData = rank.attributes[1].value;
-  console.log(rankData)
+  // console.log(rankData)
 })
 
 
